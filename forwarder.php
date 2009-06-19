fb30f51e964919c9dc7f8c4c0897819a69f9c5fd
reneade <reneade@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-19 23:08:55 +0000

    added forwarder to fit the stupid wordpress plugins folder structure
    git-svn-id: https://plugins.svn.wordpress.org/related-posts/trunk@127567 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ forwarder.php
@@ -0,0 +1,16 @@
+<?php
+//-----------------------------------------------------------------------------
+/*
+Plugin Name: Related Posts
+Version: 0.3.1
+Plugin URI: http://www.rene-ade.de/inhalte/wordpress-plugin-relatedposts.html
+Description: This wordpress plugin provides tagcloud that shows the related posts of a post, and can replace a keyword within a post to a list of related posts.
+Author: Ren&eacute; Ade
+Author URI: http://www.rene-ade.de
+Min WP Version: 2.3
+*/
+//-----------------------------------------------------------------------------
+?>
+<?php
+  include 'related-posts/related-posts.php';
+?>
--- related-posts/related-posts.php
+++ related-posts/related-posts.php
@@ -2,7 +2,7 @@
 //-----------------------------------------------------------------------------
 /*
 Plugin Name: Related Posts
-Version: 0.3
+Version: 0.3.1
 Plugin URI: http://www.rene-ade.de/inhalte/wordpress-plugin-relatedposts.html
 Description: This wordpress plugin provides tagcloud that shows the related posts of a post, and can replace a keyword within a post to a list of related posts.
 Author: Ren&eacute; Ade
