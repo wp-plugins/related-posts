a0634f76e125de42fe9df7d21f8bc501dc069526
jureham <jureham@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-08 10:51:04 +0000

    version fix
    git-svn-id: https://plugins.svn.wordpress.org/related-posts/tags/2.6@677995 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp_related_posts.php
+++ wp_related_posts.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Related Posts
-Version: 2.4.1
+Version: 2.6
 Plugin URI: http://wordpress.org/extend/plugins/related-posts/
 Description: Quickly increase your readers' engagement with your posts by adding Related Posts in the footer of your content.
 Author: Zemanta
