0dba241b05c07ca8b7b56f97607ddca1e95393d2
zemanta <zemanta@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-27 18:11:21 +0000

    v2.7.1
    git-svn-id: https://plugins.svn.wordpress.org/related-posts/tags/2.7.1@704774 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp_related_posts.php
+++ wp_related_posts.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Related Posts
-Version: 2.6
+Version: 2.7.1
 Plugin URI: http://wordpress.org/extend/plugins/related-posts/
 Description: Quickly increase your readers' engagement with your posts by adding Related Posts in the footer of your content. Click on <a href="admin.php?page=wordpress-related-posts">Related Posts tab</a> to configure your settings.
 Author: Zemanta
@@ -275,10 +275,20 @@ function wp_rp_generate_related_posts_list_items($related_posts, $selected_relat
 
 		if ($platform_options["display_excerpt"]){
 			$excerpt_max_length = $platform_options["excerpt_max_length"];
-			if($related_post->post_excerpt){
-				$output .= '<br /><small>' . (mb_substr(strip_shortcodes(strip_tags($related_post->post_excerpt)), 0, $excerpt_max_length)) . '...</small>';
-			} else {
-				$output .= '<br /><small>' . (mb_substr(strip_shortcodes(strip_tags($related_post->post_content)), 0, $excerpt_max_length)) . '...</small>';
+			$excerpt = '';
+
+			if ($related_post->post_excerpt){
+				$excerpt = strip_shortcodes(strip_tags($related_post->post_excerpt));
+			}
+			if (!$excerpt) {
+				$excerpt = strip_shortcodes(strip_tags($related_post->post_content));
+			}
+
+			if ($excerpt) {
+				if (strlen($excerpt) > $excerpt_max_length) {
+					$excerpt = mb_substr($excerpt, 0, $excerpt_max_length - 3) . '...';
+				}
+				$output .= '<br /><small>' . $excerpt . '</small>';
 			}
 		}
 		$output .=  '</li>';
@@ -477,12 +487,9 @@ function wp_rp_get_related_posts($before_title = '', $after_title = '') {
 	}
 
 	$posts_footer = '';
-	if ($options['display_zemanta_linky'] || $remote_recommendations) {
+	if ($options['display_zemanta_linky']) {
 		$posts_footer = '<div class="wp_rp_footer">' .
-				(current_user_can('edit_posts')
-					? '<a class="wp_rp_edit" id="wp_rp_edit_related_posts" href="#" id="wp_rp_edit_related_posts">Edit Related Posts</a>'
-					: ($options['display_zemanta_linky'] ? '<a class="wp_rp_backlink" target="_blank" rel="nofollow" href="http://www.zemanta.com/?gp-related-posts">Zemanta</a>' : '')
-				) .
+					'<a class="wp_rp_backlink" target="_blank" rel="nofollow" href="http://www.zemanta.com/?gp-related-posts">Zemanta</a>' .
 			'</div>';
 	}
 
